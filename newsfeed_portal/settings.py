4d14319bcfe9f3022a36a1cb43bdf13ba13b102a
Saiful Islam <saiful.abir20@gmail.com>
2021-04-06 21:27:33 +0600

    settings app created for users

--- newsfeed_portal/settings.py
+++ newsfeed_portal/settings.py
@@ -76,10 +76,28 @@ WSGI_APPLICATION = 'newsfeed_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newsfeed',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+            'isolation_level': "repeatable read",
+        },
+        'CHARSET':'utf8',
+        'COLLATION':'utf8_general_ci',
+        'COLLATION_CONNECTION':'utf8_general_ci'
     }
 }
 
