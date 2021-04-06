0c3874a2752bbe525e6d3e64ddcfebed50077e09
Saiful Islam <saiful.abir20@gmail.com>
2021-04-06 22:36:45 +0600

    settings serializer created

---
+++ settings/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from settings.models import Settings
+
+
+class SettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Settings
+        fields = ('countries', 'sources', 'keywords')
