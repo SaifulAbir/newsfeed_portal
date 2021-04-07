0f16986da2e62b7eaad5bf3d45e8c242ecd92522
Saiful Islam <saiful.abir20@gmail.com>
2021-04-07 09:49:33 +0600

    news model and migration created

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from newsfeed_portal.models import newsfeedPortalModel
 
-# Create your models here.
+
+class News(newsfeedPortalModel):
+    headline = models.CharField(max_length=255, unique=True)
+    thumbnail = models.SlugField(max_length=255)
+    source_of_news = models.CharField(max_length=50)
+    country_of_news = models.CharField(max_length=255)
