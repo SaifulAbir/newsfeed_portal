45edf9d2ebe14644edb8a9584a44404165267fc2
Saiful Islam <saiful.abir20@gmail.com>
2021-04-09 20:46:09 +0600

    one task removed from celery and database default configuration added

--- newsfeed_portal/celery.py
+++ newsfeed_portal/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
-# set the default Django settings module for the 'celery' program.
 from celery.schedules import crontab
+# set the default Django settings module for the 'celery' program.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsfeed_portal.settings')
 
@@ -23,8 +23,3 @@ app.conf.beat_schedule = {
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
